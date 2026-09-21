7a8d809a017ed3e2c05bb14d6a19d969ea849c7d
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-21 11:27:15 +0530

    Add palindrome checker in palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+value = input("Enter a word or number: ")
+
+if value == value[::-1]:
+    print("It is a palindrome.")
+else:
+    print("It is not a palindrome.")
+OUTPUT:
+Enter a word or number: 5
+It is a palindrome.
